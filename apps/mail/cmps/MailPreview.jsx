2bbd379a3adfbd9a26f755647f7da0e922070c8a
Shaham Tamir <shahamt@gmail.com>
2025-04-17 12:48:22 +0300

    Add basic mail index structure

---
+++ apps/mail/cmps/MailPreview.jsx
@@ -0,0 +1,28 @@
+// === React
+// const { useState, useEffect, useRef } = React
+// const { Routes, Route, Navigate, useParams, useNavigate, Link, useSearchParams } = ReactRouterDOM
+
+// === Services
+
+// === Child Components
+
+
+
+
+// ====== Component ======
+// =======================
+
+export function MailPreview({ /* prop1, prop2 */ }) {
+    // === Hooks
+
+    // === Effects
+
+    // === Functions
+
+    // if (!data) return <div>Loading...</div>
+    return (
+        <section className="mail-preview">
+            <h1>MailPreview</h1>
+        </section>
+    )
+}
---
+++ apps/mail/cmps/MailSearchBar.jsx
@@ -0,0 +1,28 @@
+// === React
+// const { useState, useEffect, useRef } = React
+// const { Routes, Route, Navigate, useParams, useNavigate, Link, useSearchParams } = ReactRouterDOM
+
+// === Services
+
+// === Child Components
+
+
+
+
+// ====== Component ======
+// =======================
+
+export function MailSearchBar({ /* prop1, prop2 */ }) {
+    // === Hooks
+
+    // === Effects
+
+    // === Functions
+
+    // if (!data) return <div>Loading...</div>
+    return (
+        <section className="mail-search-bar">
+            <h1>MailSearchBar</h1>
+        </section>
+    )
+}

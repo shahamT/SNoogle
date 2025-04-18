1f991e4f1f47419ff9317e1f32da211abea00977
Shaham Tamir <shahamt@gmail.com>
2025-04-18 10:33:38 +0300

    Add dynamic header basics

--- cmps/app/MainHeader.jsx
+++ cmps/app/MainHeader.jsx
@@ -1,19 +1,41 @@
 import { MainLogo } from "../general/MainLogo.jsx"
+import { MailSearchBar } from "./SearchBar.jsx"
 
 // react
-const { NavLink } = ReactRouterDOM
+const { useState, useEffect } = React
+const { NavLink, useLocation } = ReactRouterDOM
+
 
 export function MainHeader({ onSetPage }) {
+    const { pathname } = useLocation()
+    const [isSearchVisible, setIsSearchVisible] = useState(false)
+
+    useEffect(() => {
+        toggleSearchBar()
+    }, [pathname])
+
+    function toggleSearchBar() {
+        if (pathname.startsWith('/mail') || pathname.startsWith('/notes')) {
+            setIsSearchVisible(true)
+        } else {
+            setIsSearchVisible(false)
+        }
+    }
+
+
 
     return (
         <header className="main-header grid main-inline-layout ">
             <div className="main-header-content-wraper flex space-between align-center">
-                <MainLogo/>
+                <MainLogo />
+
+                {isSearchVisible && <MailSearchBar />}
+
                 <nav className="main-nav">
                     <ul className="flex space-between clean-list">
                         <li> <NavLink to="/home">Home</NavLink> </li>
                         <li> <NavLink to="/mail/inbox">Mail</NavLink> </li>
-                        <li> <NavLink to="/note">Notes</NavLink> </li>
+                        <li> <NavLink to="/notes">Notes</NavLink> </li>
                         <li> <NavLink to="/about">About</NavLink> </li>
                     </ul>
                 </nav>
